afc2322bb07d1f4774d8406de010716d151f03e2
PMaitri <itspmaitri@gmail.com>
2026-02-13 11:32:24 +0530

    Remove forgot password link from login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -154,15 +154,7 @@ export default function Login() {
 
               {/* Password */}
               <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                  <Label htmlFor="password">Password</Label>
-                  <Link
-                    href="/forgot-password"
-                    className="text-xs text-primary hover:underline"
-                  >
-                    Forgot password?
-                  </Link>
-                </div>
+                <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
                   type="password"
