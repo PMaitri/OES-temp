d718debc7034a4e83fa89f2bb3b9540c0ff6fde7
PMaitri <itspmaitri@gmail.com>
2026-02-20 15:29:51 +0530

    Improve server.js robustness

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-import './dist/index.js';
+try {
+    console.log("Starting server from server.js...");
+    await import('./dist/index.js');
+} catch (err) {
+    console.error("FATAL STARTUP ERROR:", err);
+    // Keep process alive for a while so we can see error instead of 503
+    setTimeout(() => {
+        process.exit(1);
+    }, 30000);
+}
